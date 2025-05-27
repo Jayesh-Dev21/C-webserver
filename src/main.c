1efd01782f18c535f955325860d93cf30316a1d7
Jayesh Puri <jayeshkpuri@gmail.com>
2025-05-27 19:36:48 +0530

    adding headers for main

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+#define port 3000
+
+int main(int argc, int argv[]){
+    
+    char buff[1024];
+
+
+    return 0;
+}

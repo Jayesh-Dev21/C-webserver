74aa0884b4ba39d54ce78503070ad826a6ab781d
Jayesh Puri <jayeshkpuri@gmail.com>
2025-06-15 15:29:22 +0530

    updating the headers

--- src/headers.h
+++ src/headers.h
@@ -1,3 +1,6 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +13,7 @@
 #include <arpa/inet.h>
 #include "content_headers.h"
 #include "error_logging.h"
+#include "request_logging.h"
 
 const char* get_mime_type(const char* path);
 void log_error(const char* type, const char* message, const char* extra);
@@ -17,3 +21,5 @@ void log_error(const char* type, const char* message, const char* extra);
 void log_event_tofile(FILE* log_dest, const char* level, const char* client_ip, int client_port, const char* method, const char* path, const char* version, int status_code);
 
 void log_event_tostdout(const char* level, const char* client_ip, int client_port, const char* method, const char* path, const char* version, int status_code);
+
+#endif

c876a4fa5ec7b004b718edd119a35fafe7a243c8
Jayesh Puri <jayeshkpuri@gmail.com>
2025-06-29 00:57:34 +0530

    ^

--- src/request_logging.c
+++ src/request_logging.c
@@ -4,18 +4,18 @@
 #include <time.h>
 #include "request_logging.h"
 
-void log_event_tofile(FILE* log_dest, const char* level, const char* client_ip, int client_port,
-               const char* method, const char* path, const char* version, int status_code) {
+void log_event_tofile(FILE* log_dest, const char* level, const char* client_ip, int client_port, const char* method, const char* path, const char* version, int status_code){
 
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
     char timestamp[64];
     strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", t);
 
-    if (client_ip) {
+    if (client_ip){
         fprintf(log_dest, "[%s] %s - %s:%d - \"%s %s %s\" - %d\n",
                 level, timestamp, client_ip, client_port, method, path, version, status_code);
-    } else {
+    } 
+    else{
         fprintf(log_dest, "[%s] %s - %s\n", level, timestamp, method); // for general logs
     }
 
@@ -28,10 +28,11 @@ void log_event_tostdout(const char* level, const char* client_ip, int client_por
     char timestamp[64];
     strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", t);
 
-    if (client_ip) {
+    if (client_ip){
         fprintf(stdout, "[%s] %s - %s:%d - \"%s %s %s\" - %d\n", 
             level, timestamp, client_ip, client_port, method, path, version, status_code);
-    } else {
+    } 
+    else{
         fprintf(stdout, "[%s] %s - %s\n", level, timestamp, method); // for server events or errors
     }
 }
--- src/request_logging.h
+++ src/request_logging.h
@@ -7,7 +7,6 @@
 #include <time.h>
 
 void log_event_tostdout(const char* level, const char* client_ip, int client_port, const char* method, const char* path, const char* version, int status_code);
-void log_event_tofile(FILE* log_dest, const char* level, const char* client_ip, int client_port,
-               const char* method, const char* path, const char* version, int status_code);
+void log_event_tofile(FILE* log_dest, const char* level, const char* client_ip, int client_port, const char* method, const char* path, const char* version, int status_code);
 
 #endif
